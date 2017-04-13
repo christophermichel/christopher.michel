5546545da4653a2aa39493cf02762879428e863b
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-04-13 19:02:30 -0300

    Feat(AulaDeQuinta)
    implementação das classes Genero, categoria, armadura e testes

--- modulo1_cavaleiros_do_zodiaco/Saint.java
+++ modulo1_cavaleiros_do_zodiaco/Saint.java
@@ -2,15 +2,28 @@ public class Saint {
     private String nome;
     private Armadura armadura;
     private boolean armaduraVestida;
+    private Genero genero = Genero.NAOINFORMADO;
     
-    public Saint(String nome, Armadura armadura) {
+    public Saint(String nome, Armadura armadura, Genero genero) {
         this.nome = nome;
         this.armadura = armadura;
+        this.genero = genero;
     }
 
     public void vestirArmadura() {
         this.armaduraVestida = true;
     }
     
+    // camelCase
+    public boolean getArmaduraVestida() {
+        return this.armaduraVestida;
+    }
     
+    public Genero getGenero() {
+        return this.genero;
+    }
+    
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
 }
