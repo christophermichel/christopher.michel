4e34112f4ed3e5f238be04f28f9cd7ad716b5d8f
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-04-21 19:11:36 -0300

    refactor(ListaSaint): retirar duplicidade de código no método ordenar.

--- modulo1_cavaleiros_do_zodiaco/ListaSaint.java
+++ modulo1_cavaleiros_do_zodiaco/ListaSaint.java
@@ -108,53 +108,38 @@ public class ListaSaint {
         return exemploSaint;
     }
 
-    public void ordenar(){
+   public void ordenar(TipoOrdenacao tipoOrdenacao) {
+        /*
+         * BubbleSort
+         * Complexidade: O(n^2)
+         * 
+         * 
+         *     [4] [3] [60] [17] [10]
+         * i0: [3] [4] [17] [10] [60]
+         * i1: [3] [4] [10] [17] [60]
+         */
+        boolean ascendente = tipoOrdenacao == TipoOrdenacao.ASCENDENTE;
         boolean posicoesSendoTrocadas;
         do {
             posicoesSendoTrocadas = false;
             for (int i = 0; i < this.saints.size() - 1; i++) {
                 Saint atual = this.saints.get(i);
                 Saint proximo = this.saints.get(i + 1);
-                boolean precisaTrocar = atual.getVida() > proximo.getVida();
+                boolean precisaTrocar = 
+                    ascendente ? atual.getVida() > proximo.getVida() :
+                    atual.getVida() < proximo.getVida();
+
                 if (precisaTrocar) {
                     this.saints.set(i, proximo);
                     this.saints.set(i + 1, atual);
                     posicoesSendoTrocadas = true;
                 }
             }
-        } while (posicoesSendoTrocadas);   
-        // Saint saint = null;
-        // for(int i = 0; i < this.saints.size(); i++){
-        // for(int j = 1; j < this.saints.size(); j++){
-        // if ( saints.get(i).getVida() > saints.get(j).getVida()){
-        // saint = saints.get(i);
-        // saints.add(i, saints.get(j));
-        // saints.add(j, saint);
-        // }
-        // }        
-        // }
-    }   
+        } while (posicoesSendoTrocadas); 
+    }
 
-    public void ordenar(TipoOrdenacao tipoOrdenacao) {
-
-        if (tipoOrdenacao.equals(TipoOrdenacao.DESCENDENTE)) {
-            boolean posicoesSendoTrocadas;
-            do {
-                posicoesSendoTrocadas = false;
-                for (int i = 0; i < this.saints.size() - 1; i++) {
-                    Saint atual = this.saints.get(i);
-                    Saint proximo = this.saints.get(i + 1);
-                    boolean precisaTrocar = atual.getVida() > proximo.getVida();
-                    if (precisaTrocar) {
-                        this.saints.set(i, proximo);
-                        this.saints.set(i + 1, atual);
-                        posicoesSendoTrocadas = true;
-                    }
-                }
-            } while (posicoesSendoTrocadas);  
-        } else {
-            ordenar();
-        }
+    public void ordenar() {
+        this.ordenar(TipoOrdenacao.ASCENDENTE);
     }
 
     public ArrayList<Saint> unir (ArrayList listaNova) {
@@ -173,9 +158,25 @@ public class ListaSaint {
         }
         for (k=0; k<listaNova.size(); k++) {
             adicionaSaint(saints.get(k));
-
         }
         return listaUnida;
+    }
+    
+    public ArrayList<Saint> diff (ArrayList listaNova) {
+        int i, j;
+        ArrayList<Saint> listaDiferentes = new ArrayList<>();
+        for (i=0; i<saints.size(); i++) {
+            boolean encontrado = false;
+            for (j=0; j<listaNova.size(); j++) {
+                if (saints.get(i).equals(listaNova.get(j))) {
+                    encontrado = true;
+                }
+            }
+            if (encontrado) {
+                listaDiferentes.add(saints.get(i));
+            }            
+        }
+        return listaDiferentes;
     }
 
     public ArrayList<Saint> intersec (ArrayList listaNova) {
@@ -188,30 +189,13 @@ public class ListaSaint {
                     encontrado = true;
                 }
             }
-            if (encontrado) {
+            if (!encontrado) {
                 listaIguais.add(saints.get(i));
-            }
+            }            
         }
         return listaIguais;
     }
-
-    public ArrayList<Saint> diff (ArrayList listaNova) {
-        int i, j;
-        ArrayList<Saint> listaDiferentes = new ArrayList<>();
-        for (i=0; i<saints.size(); i++) {
-            boolean encontrado = false;
-            for (j=0; j<listaNova.size(); j++) {
-                if (saints.get(i).equals(listaNova.get(j))) {
-                    encontrado = true;
-                }
-            }
-            if (!encontrado) {
-                listaDiferentes.add(saints.get(i));
-            }
-        }
-        return listaDiferentes;
-    }
-
+ 
     public String getCSV(){
         StringBuilder stringCSV = new StringBuilder();
         int i;
