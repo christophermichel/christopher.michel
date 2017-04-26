f2c6544f480b172505b7c6bb28a8228022301d98
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-04-26 01:14:38 -0300

    feat(HobbitContador): criar classe e implementar método que retorna o soma da diferença do produto pelo MMC de uma ArrayList contendo pares.

---
+++ Hobbit/HobbitContador.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class HobbitContador {
+
+    private int produtoMenosMMC = 0;
+    
+    public int soma(ArrayList<ArrayList<Integer>> arrayDePares) {
+        ArrayList<Integer> resultados = new ArrayList<>();
+        int resultado;
+        int MMC;
+        for(int i=0;i<arrayDePares.size();i++) {
+            ArrayList<Integer> par = arrayDePares.get(i);
+            boolean achouMMC = false;
+            int produto = par.get(0) * par.get(1);
+            int j=1;
+            while (!achouMMC) {
+                if (j%par.get(0)== 0 && j!=par.get(0) && j%par.get(1)==0 && j!=par.get(1)) {
+                    MMC = j;
+                    achouMMC = true;
+                    resultado = produto - MMC;
+                    resultados.add(resultado);
+                } else {
+                    j++;
+                }
+            }
+        }
+        
+        for(int k=0; k<resultados.size();k++) {
+            produtoMenosMMC = produtoMenosMMC + resultados.get(k);
+        }
+    return produtoMenosMMC;
+    }
+}
