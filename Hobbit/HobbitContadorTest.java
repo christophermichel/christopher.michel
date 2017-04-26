c10ade20c4faf12d69cde3d026f40f6bdc0e78e8
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-04-26 13:59:04 -0300

    fix(HobbitContadorTest): alterar nome de método nos testes.

--- Hobbit/HobbitContadorTest.java
+++ Hobbit/HobbitContadorTest.java
@@ -11,49 +11,50 @@ public class HobbitContadorTest {
         HobbitContador bilbo = new HobbitContador("Bilbo");
         ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
         arrayDePares.add(new ArrayList<>(Arrays.asList(2, 3)));
-        
+
         assertEquals(0, bilbo.calculaDiferenca(arrayDePares));
     }
-    
+
     @Test
     public void calculaDiferencaCom2Pares() {
         HobbitContador bilbo = new HobbitContador("Bilbo");
         ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
         arrayDePares.add(new ArrayList<>(Arrays.asList(3, 10)));
         arrayDePares.add(new ArrayList<>(Arrays.asList(2, 3)));
-        
+
         assertEquals(0, bilbo.calculaDiferenca(arrayDePares));
     }
-    
+
     @Test
     public void calculaDiferencaSemPares() {
         HobbitContador bilbo = new HobbitContador("Bilbo");
         ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
-        
+
         assertEquals(0, bilbo.calculaDiferenca(arrayDePares));
     }
-    
+
     @Test
     public void maiorMultiploNumeroMaiorQueTres() {
         HobbitContador bilbo = new HobbitContador("Bilbo");
-        int teste = bilbo.obterMaiorMultiploDeTres(20);
-        
+        int teste = bilbo.maiorMultiploDeTres(20);
+
         assertEquals(18, teste);
     }
-    
+
     @Test
     public void maiorMultiploNumeroTres() {
         HobbitContador bilbo = new HobbitContador("Bilbo");
-        int teste = bilbo.obterMaiorMultiploDeTres(3);
-        
+        int teste = bilbo.maiorMultiploDeTres(3);
+
         assertEquals(3, teste);
     }
-    
+
     @Test
     public void maiorMultiploNumeroMenorQueTres() {
         HobbitContador bilbo = new HobbitContador("Bilbo");
-        int teste = bilbo.obterMaiorMultiploDeTres(2);
-        
+        int teste = bilbo.maiorMultiploDeTres(2);
+
         assertEquals(0, teste);
     }
+
 }
