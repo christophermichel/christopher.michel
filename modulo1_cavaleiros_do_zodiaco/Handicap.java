a3f4f05cea535c4b8ac49cb5513bacde0d359b73
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-05-09 01:56:53 -0300

    fix(analisarBuild): analisar se há algum erro na classe.

--- modulo1_cavaleiros_do_zodiaco/Handicap.java
+++ modulo1_cavaleiros_do_zodiaco/Handicap.java
@@ -1,19 +1,19 @@
-import java.util.Random; 
-public class Handicap implements Movimento{ 
-    private Saint golpeado; 
-    private Saint golpeador; 
+// import java.util.Random; 
+// public class Handicap implements Movimento{ 
+    // private Saint golpeado; 
+    // private Saint golpeador; 
  
-    public Handicap(Saint golpeador, Saint golpeado){ 
-        this.golpeado = golpeado; 
-        this.golpeador = golpeador; 
+    // public Handicap(Saint golpeador, Saint golpeado){ 
+        // this.golpeado = golpeado; 
+        // this.golpeador = golpeador; 
  
-    } 
+    // } 
  
-    public void executar(){ 
-        Sorteador comDadoD6 = new DadoD6(); 
-        SortearContraAtaque sortear = new SortearContraAtaque(comDadoD6); 
-        if((golpeado.getVida() < 50) && golpeado.getArmaduraVestida() == false){ 
-            golpeado.vaiBloquearProximoAtaque = sortear.vaiContraAtacar(); 
-        } 
-    } 
-}
+    // public void executar(){ 
+        // Sorteador comDadoD6 = new DadoD6(); 
+        // SortearContraAtaque sortear = new SortearContraAtaque(comDadoD6); 
+        // if((golpeado.getVida() < 50) && golpeado.getArmaduraVestida() == false){ 
+            // golpeado.vaiBloquearProximoAtaque = sortear.vaiContraAtacar(); 
+        // } 
+    // } 
+// }
