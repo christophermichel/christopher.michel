aeeae7054fe99a909c40ba61a80f896010edbe79
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-05-12 01:17:54 -0300

    feat(Lista1JS): implementar exercícios 5 e 6 INCOMPLETO.

--- modulo-04/lista-js-basico/JS/lista-js-basico.js
+++ modulo-04/lista-js-basico/JS/lista-js-basico.js
@@ -44,3 +44,52 @@ function somaDiferentona(a) {
 
 console.log("Soma Diferentona de (3)(4) é: ",somaDiferentona(3)(4));
 console.log("Soma Diferentona de (5642)(8749) é: ",somaDiferentona(5642)(8749));
+
+//Exercício 5
+
+function fibonacci(a){
+    if(a<0){
+      console.log("Número inválido")
+    }
+    if(a === 2 || a === 1) {
+      return 1
+    }
+    else {
+      return (fibonacci(a-1) + fibonacci(a-2))
+    }
+}
+
+/*function fiboSum(numeroDeTermos){
+  var soma = 0;
+  for(var i = 1, i<numeroDeTermos, i++) {
+    soma = soma + fibonacci(i);
+  }
+  return soma
+}*/
+
+//Exercício 6
+
+function bubbleSort(a) {
+    var trocarposicoes;
+    var a = a;
+    do {
+        trocarposicoes = false;
+        for (var i=0; i < a.length-1; i++) {
+            if (a[i] > a[i+1]) {
+                var temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+                trocarposicoes = true;
+            }
+        }
+    } while (trocarposicoes);
+}
+
+function queroCafe (mascada, precos)
+  var precos = precos;
+  for(i=0, i<precos.length, i++) {
+    if(precos[i] <= mascada)
+        possiveis.push(precos[i]);
+  }
+  return bubbleSort(possiveis).toString();
+}
