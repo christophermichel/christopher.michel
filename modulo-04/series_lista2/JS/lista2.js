2b95450e7a1d2e69011a05268b73db8b97bd5927
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-05-14 01:06:41 -0300

    feat(Lista2JS): implementar exercícios 4 e 5.

--- modulo-04/series_lista2/JS/lista2.js
+++ modulo-04/series_lista2/JS/lista2.js
@@ -3,10 +3,10 @@ function seriesInvalidas(series) {
   var seriesQueNaoDevemAparecer = new Array;
   series
   .forEach(i => {
-		 if(i.anoEstreia > new Date().getFullYear()) {
-		 	seriesQueNaoDevemAparecer.push(i.titulo)
-			}
-		});
+     if(i.anoEstreia > new Date().getFullYear()) {
+       seriesQueNaoDevemAparecer.push(i.titulo)
+      }
+    });
   return "Séries Inválidas: " + seriesQueNaoDevemAparecer.join(" - ");
 }
 console.log("Exercicio 1: " + seriesInvalidas(series));
@@ -14,21 +14,55 @@ console.log("Exercicio 1: " + seriesInvalidas(series));
 
 
 //Exercício 2
-function seriesPorAno(serie,ano) {
-	return series.filter(series => (series.anoEstreia > ano))
+function seriesPorAno(series,ano) {
+	var seriesSelecionadas = Array();
+  for(serie of series) {
+		if(serie.anoEstreia >= ano) {
+			seriesSelecionadas.push(serie)
+		}
+	}
+	return seriesSelecionadas;
 }
-
-console.log('exercicio 02 ');
-console.log(seriesPorAno(series,2015));
+console.log("Exercício 2" + seriesPorAno(series,2015));
 
 //Exercício 3
 
 function mediaDeEpisodios(series) {
-	var somaDeEpisodios = 0;
-	var media = 0;
-	series.forEach(function(serie){
-		somaDeEpisodios += (serie.numeroEpisodios)
-	})
-	return media = somaDeEpisodios/series.length
+  var somaDeEpisodios = 0;
+  var media = 0;
+  series.forEach(function(serie){
+    somaDeEpisodios += (serie.numeroEpisodios)
+  })
+  return media = somaDeEpisodios/series.length
 }
-console.log(mediaDeEpisodios(series))
+console.log("Exercício 3: " + mediaDeEpisodios(series))
+
+//Exercício 4
+
+function procurarPorNome(series,nome){
+		var nomeEncontrado = false;
+		for(serie of series){
+				for(atores of serie.elenco){
+						if(nome === atores){
+								nomeEncontrado = true;
+								break;
+						}
+				}
+		}
+		return nomeEncontrado;
+}
+console.log("Exercício 4: Teste true - " + procurarPorNome(series,"Christopher Michel"))
+console.log("Exercício 4: Teste false - " + procurarPorNome(series,"Christopher"))
+
+//Exercício 5
+
+function MascadaEmSerie(serie) {
+	var pagamentoFinal;
+	for(atores of serie.eleco){
+		pagamentoFinal += 40000;
+	}
+	for(atores of serie.diretor){
+		pagamentoFinal += 100000;
+	}
+	return pagamentoFinal;
+}
