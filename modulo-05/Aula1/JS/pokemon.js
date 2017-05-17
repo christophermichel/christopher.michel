d4a0dab5cf3170a64cc54f9ce261087a5a0ece38
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-05-17 17:35:26 -0400

    feat(InicioExercício2): iniciar exercício 2.

--- modulo-05/Aula1/JS/pokemon.js
+++ modulo-05/Aula1/JS/pokemon.js
@@ -3,3 +3,7 @@ var aula1 = angular.module('aula1',[]);
 aula1.controller('a', function($scope) {
   $scope.pokemon = {nome: "Default", tipo: "Default"};
 });
+
+aula1.controller('b', function($scope) {
+  $scope.pokemons = [{nome: "Default", tipo: "Default"}, {nome: "Default", tipo: "Default"}, {nome: "Default", tipo: "Default"}];
+});
