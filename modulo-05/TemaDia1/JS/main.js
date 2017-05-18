d526a2711608a967c197ebca7c9e3ba10eef6423
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-05-18 02:06:16 -0300

    feat(TemaDia1): implementar filtro em lista de pokémons.

---
+++ modulo-05/TemaDia1/JS/main.js
@@ -0,0 +1,18 @@
+var modulo = angular.module('diaUm',[]);
+
+modulo.controller('listaPokemon', function($scope){
+    $scope.topoDaPagina = "Busca Pokémon"
+    $scope.procurarPokemon = "";
+    $scope.pokemons = [
+      {nome:'Bulbasaur', tipo:'grama'},
+      {nome:'Charmander', tipo:'fogo'},
+      {nome:'Wartortle', tipo:'agua'},
+      {nome:'Charizard', tipo:'fogo'},
+      {nome:'Abra', tipo:'psiquico'},
+      {nome:'Vulpix', tipo:'fogo'},
+      {nome:'Gengar', tipo:'fantasma'},
+      {nome:'Squirtle', tipo:'agua'},
+      {nome:'Lapras', tipo:'agua'},
+      {nome:'Caterpie', tipo:'venenoso'}
+    ];
+})
