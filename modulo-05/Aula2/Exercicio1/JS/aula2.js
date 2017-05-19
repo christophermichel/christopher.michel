c10a2c0ecbb9c78d39896b0265064ebda0dd9dee
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-05-19 01:03:52 -0300

    fix(Aula2): exercício 1 funcionando.

--- modulo-05/Aula2/Exercicio1/JS/aula2.js
+++ modulo-05/Aula2/Exercicio1/JS/aula2.js
@@ -1,19 +1,7 @@
-var aula2 = angular.module('aula2',[]);
+var modulo = angular.module("modulo",[]);
 
-aula2.controller('buscarPorData', function($scope, $filter) {
-  $scope.dataRecebida = "";
-  $scope.funcaoData = funcaoData;
-  $scope.data = data;
-
-  var stringData = function (dataRecebida) {
-    var dia = stringData.slice(0,2);
-    var mes = stringData.slice(3,5);
-    var ano = stringData.slice(6,10);
-    funcaoData(mes, dia, ano)
-  }
-
-  var funcaoData = function(a, b, c) {
-    var array = [b, a, c];
-    var stringDaData = a.split(.);
-    $scope.data = new Date(stringDaData);
-});
+modulo.controller ("aula2", function ($scope) {
+    $scope.convert = function () {
+        $scope.dataSaida = new Date ($scope.dataEntrada);
+    }
+})
