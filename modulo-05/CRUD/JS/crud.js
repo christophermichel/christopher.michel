60781200723907376a9a0944036843b9a0b1ec1a
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-05-22 05:07:33 -0300

    refactor(CRUD): estrutural de melhor forma e implementar os métodos para instrutores. Método incluir instrutores ainda com falha.

--- modulo-05/CRUD/JS/crud.js
+++ modulo-05/CRUD/JS/crud.js
@@ -1,37 +1,158 @@
-var modulo = angular.module('modulo', []);
+var modulo = angular.module(`modulo`,[]);
 
-modulo.controller('aulas', function($scope) {
-    $scope.aulas = [];
-    var id = 0;
+modulo.controller('aulas', function($scope){
+  $scope.aulas = aulas;
+  $scope.instrutores = instrutores;
 
-//adicionar aula no array aulas, porém o primeiro ele não
-//aceita as condições dadas para ser uma aula válida
-    $scope.adicionar = function(nome)
-    {
-        var nomes = $scope.aulas.map(aula => aula.nome);
-        if (nomes.some(i => i === nome))
-        {
-            console.log('Já existe uma aula com o nome digitado');
-            return;
+
+
+//incluir novo instrutor
+  $scope.incluirInstrutor = function (){
+      if($scope.novoInstrutorForm.$valid){
+        let jaCadastrado = false;
+        let emailInvalido = false;
+        for(cadaUm of instrutores){
+          if(cadaUm.nome === $scope.novoInstrutor.nome &&
+             cadaUm.sobrenome === $scope.novoInstrutor.sobrenome)
+            jaCadastrado = true;
+          if(cadaUm.email === $scope.novoInstrutor.email &&
+             $scope.novoInstrutor.email != null )
+             emailInvalido = true;
+          else {
+          $scope.novoInstrutor.id = instrutores.length;
+          var arrayDeAulas = [];
+          if($scope.novoInstrutor.aula != null)
+            for(a of aulas)
+              for(cadaUm of $scope.novoInstrutor.aula){
+                if(a.nome === cadaUm){
+                  arrayDeAulas.push(a);
+              }
+          }
+          $scope.novoInstrutor.aula = arrayDeAulas;
+          instrutores.push($scope.novoInstrutor);
+          $scope.novoInstrutor = {};
+          
         }
-        if ($scope.AulasForm.$valid)
-        {
-            $scope.aulas.push({id, nome});
-            $scope.novaAula = { };
-            id++;
         }
+      }
+  }
+
+
+//alterar instrutor já existente
+  $scope.alterarInstrutor = function (){
+      if($scope.alterarInstrutorExistente != undefined){
+        var indexAtual;
+        for (cadaUm of instrutores){
+          if(cadaUm.nome === $scope.selectAltera)
+            indexAtual = instrutores.indexOf(cadaUm);
+          }
+            instrutores[indexAtual].nome = $scope.alterarInstrutorExistente.nome;
+            instrutores[indexAtual].sobrenome = $scope.alterarInstrutorExistente.sobrenome;
+            instrutores[indexAtual].idade = $scope.alterarInstrutorExistente.idade;
+            instrutores[indexAtual].email = $scope.alterarInstrutorExistente.email;
+            instrutores[indexAtual].dandoAula = $scope.alterarInstrutorExistente.dandoAula;
+            instrutores[indexAtual].aula = $scope.alterarInstrutorExistente.aula;
+            instrutores[indexAtual].urlFoto = $scope.alterarInstrutorExistente.urlFoto;
+          $scope.alterarInstrutorExistente = {};
+          alert("Alterações realizadas com sucesso!");
     }
-//Alterar aula já existente
-    $scope.alterar = function(id, novoNome)
-    {
-        $scope.aulas[id].nome = novoNome;
-        $scope.novoNome = '';
+  }
+
+//deletar instrutor
+    $scope.deletarInstrutor = function (){
+      let dandoAula = false;
+      if($scope.deletarInstrutorForm.$valid){
+        for (cadaUm of instrutores){
+          let indexAtual = instrutores.indexOf(cadaUm);
+          if(cadaUm.nome === $scope.instrutorDeletar){
+                instrutores.splice(indexAtual, 1);
+                alert("Instrutor deletado com sucesso!");
+          }
+        }
+      }
     }
 
-//Excluir aula
-    $scope.excluir = function(id)
+//Incluir nova aula
+  $scope.incluirAula = function ()
+  {
+    if($scope.aulasForm.$valid)
     {
-        console.log(id);
-        $scope.aulas.splice(id, 1);
+      var aulaExiste = true;
+      for(a of aulas)
+      {
+        if(a.nome === $scope.novaAula.nome)
+          aulaExiste = false;
+      }
+      if(aulaExiste)
+      {
+        $scope.novaAula.id = aulas.length;
+        aulas.push($scope.novaAula);
+        $scope.novaAula = {};
+        alert("Aula incluida com sucesso!");
+      }
+    } else
+    {
+      alert("Não foi possível incluir a aula")
     }
+  }
+
+
+//alterar aula já existente
+  $scope.alterarAula = function(){
+    aulas[$scope.selecionarAula.id].nome = $scope.alterarAula.nome;
+    alert("Aula modificada com sucesso!");
+  }
+
+// deletar aula
 });
+
+
+
+modulo.filter('ordenarPorAula', function(){
+  return function(){
+    return aulas.sort(function(a, b) {
+          if (a.nome.toUpperCase() < b.nome.toUpperCase()) {
+            return -1;
+          }
+          if (a.nome.toUpperCase() > b.nome.toUpperCase()) {
+            return 1;
+          }
+          return 0;
+        });
+  }
+})
+modulo.filter('ordenarPorInstrutor', function(){
+  return function(){
+    return instrutores.sort(function(a, b) {
+          if (a.nome.toUpperCase() < b.nome.toUpperCase()) {
+            return -1;
+          }
+          if (a.nome.toUpperCase() > b.nome.toUpperCase()) {
+            return 1;
+          }
+          return 0;
+        });
+  }
+})
+//aula para exemplo
+let aulas = [
+  { id: 0,
+    nome:'Orientacao a Objetos'
+  },
+  {
+    id: 1,
+    nome:'Banco de Dados'
+  }
+];
+// Instrutor para exemplo
+let instrutores = [
+  { id: 0,
+    nome: 'Bernardo',
+    sobrenome: 'Rezende',
+    idade: 30,
+    email: 'bernardo@cwi.com.br',
+    jaDeuAula: true,
+    aula: [aulas[0], aulas[1]],
+    urlFoto: 'xxx'
+  }
+];
