38aa66d67bab7db7ba14f00a9a0f6923999a31be
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-05-28 14:37:58 -0300

    feat(Chat): inicio backEnd.

---
+++ modulo-06/DEV/ChatCrescer/backend/chat_crescer/chat_crescer/Controllers/MensagemController.cs
@@ -0,0 +1,40 @@
+﻿using chat_crescer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace chat_crescer.Controllers
+{
+    public class MensagemController : ApiController
+    {
+        public static List<Mensagem> Mensagens = new List<Mensagem>();
+        private static int IdMensagem = 0;
+        private object locker = new object();
+
+        public IEnumerable<Mensagem> Get()
+        {
+            return Mensagens;
+        }
+
+        public IHttpActionResult Post(Mensagem mensagem)
+        {
+            if (mensagem == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                lock (locker)
+                {
+                    Mensagens.Add(mensagem);
+                    mensagem.Id = ++IdMensagem;
+                    mensagem.HoraDoEnvio = DateTime.Now;
+                    return Ok(mensagem);
+                }
+            }
+        }
+    }
+}
---
+++ modulo-06/DEV/ChatCrescer/backend/chat_crescer/chat_crescer/Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+﻿using chat_crescer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Chat.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        public static List<Usuario> Usuarios = new List<Usuario>();
+        private static int IdUsuario = 0;
+        private object locker = new object();
+
+        public IEnumerable<Usuario> Get()
+        {
+            return Usuarios;
+        }
+
+        public IHttpActionResult Post(Usuario usuario)
+        {
+            if (usuario == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                lock (locker)
+                {
+                    Usuarios.Add(usuario);
+                    usuario.Id = ++IdUsuario;
+                    return Ok(usuario);
+                }
+            }
+        }
+    }
+}
