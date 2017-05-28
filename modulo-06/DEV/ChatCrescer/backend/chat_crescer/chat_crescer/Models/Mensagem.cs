6d796a88aac95543977a364876f62fe2c9b4ac02
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-05-28 14:53:08 -0300

    feat(Chat): inicio backEnd.

---
+++ modulo-06/DEV/ChatCrescer/backend/chat_crescer/chat_crescer/Models/Mensagem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chat_crescer.Models
+{
+    public class Mensagem
+    {
+        public int Id { get; set; }
+        public Usuario usuario { get; set; }
+        public string Texto { get; set; }
+        public DateTime HoraDeEnvio { get; set; }
+    }
+}
