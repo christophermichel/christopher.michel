7ff83adced465412295301bc5d8f0cfe03360b03
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-05-29 17:45:13 -0300

    feat(Aula4): modificar dados no banco.

---
+++ modulo-06/DEV/Aula4 - ado/Demo1/Demo1.Infraestrutura/Repositorios/Produtorepositorio.cs
@@ -0,0 +1,83 @@
+﻿using Demo1.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.Infraestrutura.Repositorios
+{
+    public class ProdutoRepositorio
+    {
+        string stringConexao = "Server=13.65.101.67;User Id=christopher.michel;Password=123456;Database=aluno11db";
+
+        public void Criar(Produto produto)
+        {
+            
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText =
+                        @"INSERT INTO Produto (Nome, Preco, Estoque) 
+                        VALUES (@nome, @preco, @estoque)";
+
+                    comando.Parameters.AddWithValue("@nome", produto.Nome);
+                    comando.Parameters.AddWithValue("@preco", produto.Preco);
+                    comando.Parameters.AddWithValue("@estoque", produto.Estoque);
+
+                    // Executa o comando e retorna somente a quantidade de linhas afetadas
+                    comando.ExecuteNonQuery();
+
+                    // Lê o resultado de um SELECT
+                    // comando.ExecuteReader();
+
+                    // Executa o comando e retorna o primeiro resultado
+                    // comando.ExecuteScalar();
+                }
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "SELECT @@IDENTITY";
+                    var result = (decimal)comando.ExecuteScalar();
+                    // Executa o comando e retorna o primeiro resultado
+                    produto.Id = (int)result;
+                }
+            }
+        }
+
+        public List<Produto> Listar()
+        {
+            var produtos = new List<Produto>();
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText =
+                        "SELECT Id, Nome, Preco, Estoque FROM Produto";
+
+                    var dataReader = comando.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        var produto = new Produto();
+
+                        produto.Id = (int)dataReader["Id"];
+                        produto.Nome = (string)dataReader["Nome"];
+                        produto.Preco = (decimal)dataReader["Preco"];
+                        produto.Estoque = (int)dataReader["Estoque"];
+
+                        produtos.Add(produto);
+                    }
+                }
+            }
+
+            return produtos;
+        }
+    }
+}
--- modulo-06/DEV/Aula4 - ado/Demo1/Demo1.WepApi/Controllers/ProdutosController.cs
+++ modulo-06/DEV/Aula4 - ado/Demo1/Demo1.WepApi/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
-﻿using Demo1.WepApi.Models;
+﻿using Demo1.Dominio.Entidades;
+using Demo1.Infraestrutura.Repositorios;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -11,52 +12,21 @@ namespace Demo1.WepApi.Controllers
 {
     public class ProdutosController : ApiController
     {
+        ProdutoRepositorio _produtorepositorio = new ProdutoRepositorio();
+
         public IHttpActionResult Post(Produto produto)
         {
-            if (produto.Estoque < 1)
-                return BadRequest("Estoque deve ser maior que zero.");
+            var mensagens = new List<string>();
+            if (!produto.Validar(out mensagens))
+                return BadRequest(string.Join(".", mensagens.ToArray()));
 
-            if (produto.Preco < 0.01M)
-                return BadRequest("Preço deve ser maior que zero.");
-
-            if (string.IsNullOrWhiteSpace(produto.Nome))
-                return BadRequest("Nome deve ser informado.");
-
-            string stringConexao = "Server=13.65.101.67;User Id=christopher.michel;Password=123456;Database=aluno11db";
-
-            using (var conexao = new SqlConnection(stringConexao))
-            {
-                conexao.Open();
-
-                using (var comando = conexao.CreateCommand())
-                {
-                    comando.CommandText =
-                        @"INSERT INTO Produto (Nome, Preco, Estoque) 
-                        VALUES (@nome, @preco, @estoque)";
-
-                    comando.Parameters.AddWithValue("@nome", produto.Nome);
-                    comando.Parameters.AddWithValue("@preco", produto.Preco);
-                    comando.Parameters.AddWithValue("@estoque", produto.Estoque);
-
-                    // Executa o comando e retorna somente a quantidade de linhas afetadas
-                    comando.ExecuteNonQuery();
-
-                    // Lê o resultado de um SELECT
-                    // comando.ExecuteReader();
-
-                    // Executa o comando e retorna o primeiro resultado
-                    // comando.ExecuteScalar();
-                }
-
-                using (var comando = conexao.CreateCommand())
-                {
-                    comando.CommandText = "SELECT @@IDENTITY";
-                    var result = (decimal)comando.ExecuteScalar();
-                    // Executa o comando e retorna o primeiro resultado
-                    produto.Id = (int)result;
-                }
-            }
             return Ok(produto);
+        }
+
+        public IHttpActionResult Get()
+        {
+            var produtos = _produtorepositorio.Listar();
+            return Ok(produtos);
         }
     }
 }
