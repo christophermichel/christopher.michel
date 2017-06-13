91048f8fdfeb5567c8fd3fdc5d746fa3a1d81867
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-06-13 15:04:57 -0300

    feat(BancoDeDadosII): exercício 3, lista 1.

--- modulo-07/Aula1/CRESCER 2017 - Aula1.sql
+++ modulo-07/Aula1/CRESCER 2017 - Aula1.sql
@@ -16,7 +16,7 @@ commit
 select * from pedido
 select * from pedidoitem
 
-
+create view vwProdutos_sem_compra as
 SELECT IDProduto, Nome
 FROM Produto 
 WHERE IDProduto NOT IN
@@ -24,14 +24,31 @@ WHERE IDProduto NOT IN
 FROM PedidoItem pit
 INNER JOIN Pedido ped
 ON pit.IDPedido = ped.IDPedido 
-WHERE DataPedido >= ADD_MONTHS(SYSDATE,-4)) 
+WHERE DataPedido >= ADD_MONTHS(SYSDATE,-4));
+
+select * from user_tables;
+
+EXEC dbms_stats.gather_schema_stats(USER);
 
 
 UPDATE Produto 
 SET Situacao = 'I'
-WHERE IDProduto NOT IN
+WHERE IDProduto IN
 (SELECT IDProduto 
 FROM PedidoItem pit
 INNER JOIN Pedido ped
 ON pit.IDPedido = ped.IDPedido 
 WHERE DataPedido >= ADD_MONTHS(SYSDATE,-4)) 
+
+---Usando o exists
+UPDATE Produto 
+SET Situacao = 'I'
+WHERE exists (Select IDProduto
+              from vwProdutos_Sem_Compra vw
+              where Produto.IDProduto = vw.IDProduto);
+              
+Select sum(item.Quantidade) qtde
+from PedidoItem item 
+  inner join Pedido ped on ped.IDPedido - item.IDPedido
+where item.IDProduto = :p_IDProduto
+and ped.DataPedido >= trunc(sysdate, 'yyyy');
