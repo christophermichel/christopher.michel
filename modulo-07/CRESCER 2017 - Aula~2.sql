949bc80fe0a8e891e4e8eec63a63adb867294291
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-06-14 01:08:56 -0300

    feat(BancoDeDadosII): lista 2.

--- modulo-07/CRESCER 2017 - Aula~2.sql
+++ modulo-07/CRESCER 2017 - Aula~2.sql
@@ -1,31 +1,41 @@
-SELECT NOME,UF, COUNT(*)  as quantidade
+//1
+
+DECLARE
+ CURSOR C_ListaDeCidades IS
+     SELECT NOME,UF, COUNT(*)  as quantidade
   from   CIDADE cid
   group  by  NOME,UF
-  having count(*) > 1; 
+  having count(*) > 1;
+ CURSOR C_ClientesPorCidades(cidade IN string) IS
+  SELECT cli.Nome AS NomeCliente, cid.Nome AS NomeCidade FROM Cliente cli JOIN Cidade cid ON cli.IDCIDADE = cid.IDCIDADE
+  WHERE cid.Nome LIKE cidade;
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  SELECT cli.NOME
-  FROM CLIENTE cli
-  INNER JOIN cidade cid ON cli.IDCidade = cid.IDCIDADE
-  where cid.NOME = nomeCidade AND cid.UF = nomeUF;
-  
-  BEGIN 
-    FOR cidade IN C_CidadesRepetidas LOOP
-        FOR cliente IN C_CLIENTES_DA_CIDADE(cidade.NOME, cidade.UF) LOOP
-          DBMS_OUTPUT.PUT_LINE('Nome do cliente: ' || cliente.NOME || '  Nome da Cidade: '|| cidade.NOME || '  Nome da UF: ' || cidade.UF);
-        END LOOP;
-    END LOOP;
-  END;
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('Cidades duplicadas');
+   FOR reg IN C_ListaDeCidades LOOP
+    DBMS_OUTPUT.PUT_LINE('Cidade' || reg.nome);
+    FOR item  in C_ClientesPorCidades(reg.nome) LOOP
+      DBMS_OUTPUT.PUT_LINE('Cliente - ' || item.nomeCliente);
+     END LOOP;
+   END LOOP;
+END;
+
+//2
+
+DECLARE
+  CURSOR C_PEDIDO(IDPEDIDO IN number) IS
+    SELECT QUANTIDADE , PRECOUNITARIO  
+    FROM PEDIDOITEM 
+    where IDPEDIDO = IDPEDIDO;
+  vPEDIDO PEDIDO.IDPEDIDO%TYPE;
+  vValor PEDIDO.VALORPEDIDO%TYPE;
+BEGIN
+  vPEDIDO := 30;
+  vValor := 0.0;
+  FOR x IN C_PEDIDO(vPedido) LOOP
+    vValor := vValor + ( x.quantidade * x.precounitario);
+  END LOOP;
+  UPDATE PEDIDO 
+  SET VALORPEDIDO =  vValor
+  WHERE IDPEDIDO = vPEDIDO;
+END;
