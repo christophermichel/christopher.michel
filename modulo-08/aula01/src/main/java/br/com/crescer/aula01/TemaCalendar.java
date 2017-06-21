5969dda2b901095d36f0176d2c7533c01c68d4e6
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-06-21 03:21:56 -0300

    fix(Lista1): ajustar método diaSemana para pegar posição anterior no enum (correta).

--- modulo-08/aula01/src/main/java/br/com/crescer/aula01/TemaCalendar.java
+++ modulo-08/aula01/src/main/java/br/com/crescer/aula01/TemaCalendar.java
@@ -22,17 +22,25 @@ public class TemaCalendar implements CalendarUtils{
     public static void main (String[] args) throws ParseException {
         TemaCalendar testa = new TemaCalendar();
         System.out.println(testa.diaSemana(new SimpleDateFormat("dd/MM/yyyy").parse("19/06/2015")));
+        System.out.println(testa.diaSemana(new SimpleDateFormat("dd/MM/yyyy").parse("21/06/2017")));
     }
     
     @Override
     public DiaSemana diaSemana(Date date) {
         calendar.setTime(date);
-        DiaSemana dia = DiaSemana.values()[calendar.get(Calendar.DAY_OF_WEEK)];
+        DiaSemana dia = DiaSemana.values()[calendar.get(Calendar.DAY_OF_WEEK) -1];
         return dia;
     }
 
     @Override
     public String tempoDecorrido(Date date) {
+        Calendar dataAtual = Calendar.getInstance();
+        Calendar dataInformada = Calendar.getInstance();
+        
+        dataAtual.setTime(new Date());
+        dataInformada.setTime(date);
+        
+        
         return "falta implementar";
     }
 }
