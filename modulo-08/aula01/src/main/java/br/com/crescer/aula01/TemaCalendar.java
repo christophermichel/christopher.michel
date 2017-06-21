5631964acd667be6e8ccf9b6e44db67573313e21
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-06-21 02:53:16 -0300

    feat(Lista1): implementar Meu Calendario Utils.

---
+++ modulo-08/aula01/src/main/java/br/com/crescer/aula01/TemaCalendar.java
@@ -0,0 +1,38 @@
+package br.com.crescer.aula01;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author chris
+ */
+
+public class TemaCalendar implements CalendarUtils{
+    
+    public SimpleDateFormat formatoData () {
+        //para poder instancia na hora de testar
+        SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
+        return formatoData;
+    }
+    Calendar calendar = Calendar.getInstance();
+    
+    public static void main (String[] args) throws ParseException {
+        TemaCalendar testa = new TemaCalendar();
+        System.out.println(testa.diaSemana(new SimpleDateFormat("dd/MM/yyyy").parse("19/06/2015")));
+    }
+    
+    @Override
+    public DiaSemana diaSemana(Date date) {
+        calendar.setTime(date);
+        DiaSemana dia = DiaSemana.values()[calendar.get(Calendar.DAY_OF_WEEK)];
+        return dia;
+    }
+
+    @Override
+    public String tempoDecorrido(Date date) {
+        return "falta implementar";
+    }
+}
