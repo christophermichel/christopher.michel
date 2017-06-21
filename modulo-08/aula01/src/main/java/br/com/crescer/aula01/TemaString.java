8676027bc2a6c9d9a8e17879eb347629fcace66f
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-06-21 00:32:33 -0300

    feat(Lista1): finalizar meu String Utils.

--- modulo-08/aula01/src/main/java/br/com/crescer/aula01/TemaString.java
+++ modulo-08/aula01/src/main/java/br/com/crescer/aula01/TemaString.java
@@ -1,13 +1,31 @@
 package br.com.crescer.aula01;
+
+import java.text.Normalizer;
+
 /**
  *
  * @author chris
  */
 public class TemaString implements StringUtils{
     
+    public static void main (String[] args) {
+        TemaString testa = new TemaString();
+        
+        System.out.println(testa.isEmpty(""));
+        System.out.println(testa.isEmpty("Hey"));
+        System.out.println(testa.inverter(""));
+        System.out.println(testa.inverter("Inverter"));
+        System.out.println(testa.contaVogais("ABABAB"));
+        System.out.println(testa.contaVogais("sdfslk"));
+        System.out.println(testa.contaVogais("sdfáslk"));
+        System.out.println(testa.isPalindromo("assa"));
+        System.out.println(testa.isPalindromo("asasa"));
+        
+    }
+    
     @Override
     public boolean isEmpty(String string) {
-        return string.length() <= 0;
+        return string.length() == 0 || string == null;
     }
     
     @Override
@@ -23,7 +41,6 @@ public class TemaString implements StringUtils{
           for (i = (tamanho - 1); i >= 0; i--){
           stringFinal.append(string.charAt(i));
         }
-        System.out.println(stringFinal);
         return stringFinal.toString();
         }
     }
@@ -31,7 +48,13 @@ public class TemaString implements StringUtils{
     @Override
     public int contaVogais(String string) {
         int vogais = 0;
+        string = 
+            Normalizer
+           .normalize(string, Normalizer.Form.NFD)
+           .replaceAll("[^\\p{ASCII}]", "");
+        
         string = string.toUpperCase();
+        
         for (int i = 0; i < string.length(); i++){
             char j = string.charAt(i);
             if (j == 'A' || j == 'E' || j == 'I' || j == 'O' || j == 'U')
@@ -42,6 +65,28 @@ public class TemaString implements StringUtils{
     
     @Override
     public boolean isPalindromo(String string) {
-        return string.length() <= 0;
+        string = 
+            Normalizer
+           .normalize(string, Normalizer.Form.NFD)
+           .replaceAll("[^\\p{ASCII}]", "");
+        
+        int i;
+        int tamanho = string.length()/2;
+        StringBuilder metadeInicial = new StringBuilder();
+        String metadeFinal;
+        
+        for (i = (tamanho - 1); i >= 0; i--){
+            metadeInicial.append(string.charAt(i));
+        }
+        
+        if (string.length() % 2 == 0) {
+            metadeFinal = string.substring((tamanho),string.length());
+        }
+        
+        else {
+            metadeFinal = string.substring((tamanho+1),string.length());
+        }
+
+        return metadeInicial.toString().equals(metadeFinal);
     }
 }
