49c5726e8bca86d28d8e639274a6d4578dbf76f7
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-06-23 02:41:34 -0300

    feat(Lista3): implementar leitura de arquivos .sql.

---
+++ modulo-08/aula03/src/main/java/br/com/crescer/aula03/SQLUtilsImpl.java
@@ -0,0 +1,76 @@
+package br.com.crescer.aula03;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author chris
+ */
+public class SQLUtilsImpl implements SQLUtils {
+    
+    File file;
+    
+    @Override
+    public void runFile(String filename) {
+        try {
+            file = new File(filename);
+
+            if (!file.exists()) {
+                throw new Exception("Não foi possível encontrar o arquivo");
+            }
+            if (filename.contains(".sql") || file.isFile()) {
+                
+                Reader reader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                ArrayList<String> arrayLinhas = new ArrayList<>();
+
+                bufferedReader
+                    .lines()
+                    .forEach(x -> arrayLinhas.add(x));
+            
+                try (final Connection connection = ConnectionUtils.getConnection()) {
+                    PreparedStatement preparedStatement;
+                    
+                    for(int i = 0; i < arrayLinhas.size(); i++){
+                        preparedStatement = connection.prepareStatement(arrayLinhas.get(i));
+                        preparedStatement.executeUpdate();
+                        connection.isClosed();
+                    }
+                } catch (SQLException ex) {
+                    Logger.getLogger(SQLUtilsImpl.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else {
+                throw new Exception("Não é um arquivo .sql");
+            }
+        } catch (Exception e) {
+            Logger.getLogger(SQLUtilsImpl.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    
+    public String executeQuery(String query) {
+    
+        return "oi";
+    }
+    
+    public void importCSV(File file) {
+    
+        return;
+    }
+    
+    public File importCSV(String query) {
+        File file = new File(query);
+        return file;
+    }
+}
