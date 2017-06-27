4acb6573732e43712ad26dfa61b81a67703dbd64
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-06-27 02:19:43 -0300

    feat(Aula5): aplicar exemplo sem banco.

--- modulo-08/aula05/src/main/java/br/com/crescer/aula05/PessoaServlet.java
+++ modulo-08/aula05/src/main/java/br/com/crescer/aula05/PessoaServlet.java
@@ -16,19 +16,39 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author christopher.michel
  */
-@WebServlet(name = "pessoa", urlPatterns = {"/pessoa"})
+@WebServlet(name = "pessoa", urlPatterns = {"/apessoa"})
 public class PessoaServlet extends HttpServlet {
 
     private List<String> nomes = new ArrayList<>();
     
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        try (final PrintWriter out = response.getWriter();) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        try (final PrintWriter out = resp.getWriter();) {
+
+            out.append("<!DOCTYPE html>");
+            out.append("<html>");
+            out.append("<head>");
+            out.append("<title>Java - aula05</title>");
+            out.append("<meta charset=\"UTF-8\">");
+            out.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            out.append("</head>");
+            out.append("<body>");
+
+            out.append("<form action=\"/aula05/pessoa\" method=\"POST\">\n");
+            out.append(" <input name=\"nome\" autofocus/>\n");
+            out.append(" <input type=\"submit\" value=\"Enviar\" />    \n");
+            out.append("</form>");
+
             out.append("<table class=\"table table-hover\"><thead><tr><th>Nome</th></tr></thead><tbody>");
             nomes.forEach(nome -> out.append("<tr><td>").append(nome).append("</td></tr>"));
             out.append("</tbody></table>");
+
+            out.append("</body>");
+            out.append("</html>");
         }
+
     }
 
     @Override
