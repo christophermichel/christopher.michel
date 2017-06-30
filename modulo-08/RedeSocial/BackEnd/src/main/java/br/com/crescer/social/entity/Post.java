7d9d816229769a789352a83a6138ab57283bccc1
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-06-30 11:31:55 -0300

    feat(RedeSocial): post mapping.

---
+++ modulo-08/RedeSocial/BackEnd/src/main/java/br/com/crescer/social/entity/Post.java
@@ -0,0 +1,77 @@
+package br.com.crescer.social.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Post implements Serializable {
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "SEQ_POST")
+    @SequenceGenerator(name = "SEQ_POST", sequenceName = "SEQ_POST", allocationSize = 1)
+    @Column(name = "ID")
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "ID")
+    private Usuario usuario;
+    
+    @Basic(optional = false)
+    @Column(name = "TITULO")
+    private String titulo;
+    
+    @Basic(optional = true)
+    @Column(name = "IMAGEM")
+    private String imagem;
+
+    @Basic(optional = false)
+    @Column(name = "CONTEUDO")
+    private String conteudo;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+    
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+}
